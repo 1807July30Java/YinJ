9d0b4996acaa8336e2c68b0fe84ed5f1c988a428
OmarGuz5 <omarguz225@gmail.com>
2018-08-07 16:53:54 -0400

    Omar's Update

---
+++ projectZero/src/main/java/com/revature/entities/BankAccount.java
@@ -0,0 +1,9 @@
+package com.revature.entities;
+
+public class BankAccount {
+	private String username;
+	private String accountID;
+	private String transaction;
+	
+
+}
---
+++ projectZero/src/main/java/com/revature/entities/Transaction.java
@@ -0,0 +1,8 @@
+package com.revature.entities;
+
+public class Transaction {
+	private String transaction;
+	private double amount;
+	private String accountID;
+
+}
